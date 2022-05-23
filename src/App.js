2007002bc0f6d80a01fdfd914bcb43102fac5945
Rahul Baghel <rahulbaghel6211@gmail.com>
2022-05-23 17:37:26 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
 
-function App() {
+import React, {
+  useEffect,
+  useState,
+} from 'react';
+
+import axios from 'axios';
+
+import Button from './components/Button';
+import CandidateCard from './components/CandidateCard';
+
+export default function App() {
+
+  const [loading, setLoding] = useState(true);
+
+  const [data, setData] = useState([]);
+
+  console.log("data : ", data);
+
+  const [page, setPage] = useState(1);
+
+  console.log("page :", page)
+
+  const [salarysort, setSalarySort] = useState("desc");
+
+  console.log("salary : ", salarysort);
+
+
+  useEffect(() => {
+
+
+    getData(page, salarysort);
+
+
+
+  }, [page, salarysort])
+
+
+
+  const getData = async (page, salarysort) => {
+
+    setLoding(true);
+
+    axios({
+
+      method: "get",
+      url: " https://json-server-mocker-masai.herokuapp.com/candidates",
+      params: {
+        _page: page,
+        _limit: 5,
+        _sort: "salary",
+        _order: `${salarysort}`,
+
+      }
+
+
+    }).then(res => {
+
+      setData(res.data);
+      setLoding(false);
+    }).catch(err => {
+      console.log("Data : ", err);
+      setLoding((false))
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {loading && <div id="loading-container">...Loading</div>}
+
+
+        {
+
+          salarysort === "desc" ? <>
+
+            <Button id="SORT_BUTTON" title={`Sort by Ascending Salary`} onClick={() => setSalarySort("asc")} />
+
+
+          </> :
+
+            <>
+
+              <Button id="SORT_BUTTON" title={`Sort by Descending Salary`} onClick={() => setSalarySort("desc")} />
+
+            </>
+
+        }
+
+        <Button title="PREV" id="PREV" disabled={page === 1} onClick={() => setPage(page - 1)} />
+
+        <Button id="NEXT" title="NEXT" onClick={() => setPage(page + 1)} />
+
+      </div>
+      {data.map((item) =>
+
+        <CandidateCard key={item.id}  {...item} />
+
+
+      )}
     </div>
   );
 }
-
-export default App;
